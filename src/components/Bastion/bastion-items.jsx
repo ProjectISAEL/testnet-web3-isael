2f5c58936745f5e88aee8189eb511f5422b46f1e
Ricardo Castro <95724981+soyRicardoCastro@users.noreply.github.com>
2022-03-29 21:07:41 +0200

    Update bastion-items.jsx
    button update

--- src/components/Bastion/bastion-items.jsx
+++ src/components/Bastion/bastion-items.jsx
@@ -2,7 +2,7 @@ import Input from "../../container/Input"
 import MainButton from '../Buttons/MainButton'
 import ImageBastion from '../../assets/banner/icon_bastion.png'
 import { MdHealthAndSafety } from 'react-icons/md'
-import { AiOutlineUserAdd } from 'react-icons/ai'
+import { AiOutlineUserAdd, AiFillFire } from 'react-icons/ai'
 import { BsBoxSeam } from 'react-icons/bs'
 
 const inputsBastion = [
@@ -13,6 +13,17 @@ const inputsBastion = [
   {
     id: 'tokenIdsOfInquisitors',
     label: 'inquisitors'
+  }
+]
+
+const inputsBanner = [
+  {
+    id: 'addBanners',
+    label: 'Add Banners',
+  },
+  {
+    id: 'AddBanners',
+    label: 'Token Id',
   }
 ]
 
@@ -34,21 +45,35 @@ export const BastionFooter = () => {
         <img src={ImageBastion} alt='Bastion Illustration' />
       </div>
       <div className='app__input'>
-        <div className='input__bastion'>
+        <div className='input__bastion' style={{ marginBottom: '20px' }}>
           {inputsBastion.map((items,i) => (
             <Input id={items.id} label={items.label} key={i} />
           ))}
           <MainButton id='mintBastion' text='Mint Bastion' color='rgb(225 15 15)' icon={<BsBoxSeam color='rgb(225 15 15)' size={24} />} />
         </div>
+
+        <div className='input__bastion'>
+          {inputsBanner.map((items,i) => (
+            <Input id={items.id} label={items.label} key={i} />
+          ))}
+          <MainButton id='addBannersBTN' text='Add Banners' icon={<AiOutlineUserAdd color='#ff8e00' size={24} />} />
+        </div>
+
         <div className='input__inquisitors'>
           {inputsInquisitors.map((items, i) => (
             <Input id={items.id} label={items.label} key={i} />
           ))}
           <MainButton id='addInquisitorsBTN' text='Add Inquisitors' icon={<AiOutlineUserAdd color='#ff8e00' size={24} />} />
         </div>
+
         <div className='input__health'>
-          <Input id='checkBastionHealthID' label='Token Bastion' />
-          <MainButton id='checkBastionHealth' color='#1dcc1b' text='Bastion Health' icon={<MdHealthAndSafety color='#1dcc1b' size={24} />} />
+          <Input id='tokenIdofBastionToBurn' label='Token Bastion' />
+          <MainButton
+	          id='burnBastion' 
+	          color='#f04005' 
+	          text='Burn Bastion' 
+	          icon={<AiFillFire color='#f04005' size={24} />}
+            />
         </div>
       </div>
   </div>
